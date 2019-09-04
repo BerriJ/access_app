15a19393805122a35a2656f376d5f1944535497b
Jonathan <jonathan@berrisch.biz>
2019-09-04 23:31:29 +0200

    Update preprocessing for db use

--- preprocess.R
+++ preprocess.R
@@ -2,12 +2,15 @@ library(readxl)
 library(dplyr)
 library(tibble)
 
-dataset <- read_excel("Z:/App/ZEA10021-Induktive_Statistik-SoSe_2019-1.xls",skip = 4)
-dataset <- as_tibble(dataset) %>% 
-             mutate_at(vars("Matrikelnummer"), .funs = as.numeric) %>%
-             dplyr::select("Nachname", "Vorname", "Matrikelnummer") %>%
-             rename("Name" = "Nachname", "Forename" = "Vorname", "Matr.Number" = "Matrikelnummer") %>%
-             add_column("Accepted" = FALSE, "Note" = NA, "Log" = NA, "Modified" = NA, "Shift" = NA)
+dataset <- read_excel(file.choose(),skip = 4)
 
-write.csv2(dataset, "students.csv", row.names = F)
-             
+students <- dataset %>% as_tibble() %>% 
+  mutate_at(vars("Matrikelnummer"), .funs = as.numeric) %>%
+  dplyr::select("Vorname", "Nachname", "Matrikelnummer") %>%
+  rename("name" = "Nachname", "forename" = "Vorname", "matrnumber" = "Matrikelnummer") %>%
+  add_column("accepted" = FALSE, "note" = NA, "log" = NA, "modified" = NA, "shift" = NA) %>%
+  filter_all(any_vars(!is.na(.)))
+
+dir.create("db", showWarnings = F)
+con <- dbConnect(RSQLite::SQLite(), "db/students_db")
+dbWriteTable(con, "students", students)
