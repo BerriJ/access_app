acb80b07535123e40e6d49d3fdfbd7feafe8b09a
TimoRamm <46932659+TimoRamm@users.noreply.github.com>
2019-09-05 12:01:05 +0200

    Add Shift ID

--- preprocess.R
+++ preprocess.R
@@ -12,6 +12,14 @@ students <- dataset %>% as_tibble() %>%
   add_column("accepted" = NA, "note" = NA, "log" = NA, "modified" = NA, "shift" = NA) %>%
   filter_all(any_vars(!is.na(.)))
 
+seats <-  150    # set No. of seats available
+overbook <- 0.05    # factor for overbooking (input as decimal)
+
+booking <- seats*(1+overbook)
+shift_no <- ceiling(nrow(students)/booking)
+students$shift <- rep(1:shift_no, each = booking)[1:nrow(students)]
+
+
 dir.create("db", showWarnings = F)
 con <- dbConnect(RSQLite::SQLite(), "db/students_db")
 dbWriteTable(con, "students", students)
