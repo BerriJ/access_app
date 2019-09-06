dc089844524d55db070e4b9afac038c427dc2e2e
Jonathan <jonathan@berrisch.biz>
2019-09-06 11:25:22 +0200

    Use pool instead of DBI to open connection

--- preprocess.R
+++ preprocess.R
@@ -2,6 +2,7 @@ library(readxl)
 library(dplyr)
 library(tibble)
 library(RSQLite)
+library(pool)
 
 dataset <- read_excel(file.choose(),skip = 4)
 
@@ -19,16 +20,25 @@ booking <- seats*(1+overbook)
 shift_no <- ceiling(nrow(students)/booking)
 students$shift <- rep(1:shift_no, each = booking)[1:nrow(students)]
 
+write.csv2(file = "example_students2.csv", students, row.names = F)
 
 # students$overbooked[(rep(1:2, each = (booking-seats))*(seats+1):booking)] <- "OVERBOOKED"
 
 # create summary table:
 
 stats <- data.frame(shift = c(1,2,3,4), sumstudents = c(0,0,0,0))
-
 dir.create("db", showWarnings = F)
-con <- dbConnect(RSQLite::SQLite(), "db/students_db")
+con <- dbPool(drv = RSQLite::SQLite(), dbname = "db/students_db")
 dbWriteTable(con, "students", students)
 dbWriteTable(con, "stats", stats, overwrite = T)
 
 dbReadTable(con, "stats")
+ 
+# # Use the example:
+# 
+# students <- read.csv2("students_example.csv")
+# stats <- data.frame(shift = c(1,2,3,4), sumstudents = c(0,0,0,0))
+# dir.create("db", showWarnings = F)
+# con <- dbPool(drv = RSQLite::SQLite(), dbname = "db/students_db")
+# dbWriteTable(con, "students", students)
+# dbWriteTable(con, "stats", stats)
