2bad251bcfab3a981af28516249c70d98bc61e99
Jonathan <jonathan@berrisch.biz>
2019-09-23 21:25:33 +0200

    Update QR Generator

--- QR-Generator.R
+++ QR-Generator.R
@@ -1,15 +1,36 @@
 #install.packages('qrcode')
 library(qrcode)
+library(dplyr)
+library(tibble)
+library(readxl)
 
-load("students_example.Rda")
+# Read in the list from Prüfungsamt:
 
-mat_num <- students$matrnumber 
-dir.create("qr", showWarnings = F)
+students_accepted <- read_excel(file.choose(),skip = 4)
+
+students_accepted <- students_accepted %>% dplyr::select(Matrikelnummer, Nachname, Vorname) %>%
+  tidyr::drop_na()
+
+# Read the list from moodle:
+
+students_moodle <- read.csv(file.choose(), stringsAsFactors = F)
+
+students_moodle <- students_moodle %>% dplyr::select(Nutzer, Matrikelnummer, E.Mail.Adresse) %>%
+  tidyr::drop_na()
+
+students_accepted$Matrikelnummer <- as.numeric(students_accepted$Matrikelnummer)
+
+Mails_and_Numbers <- semi_join(students_moodle, students_accepted, by = "Matrikelnummer")
+Missings <- anti_join(students_accepted,students_moodle, by = "Matrikelnummer")
+
+Full <- full_join(Mails_and_Numbers, Missings)
+
+
 before <- Sys.time()
 for (i in 1:length(mat_num)) {
-jpeg(file = paste('qr/', mat_num[i], '.jpeg', sep = ''), width = 500, height = 500)
-qrcode_gen(paste(mat_num[i]))
-dev.off()
+  jpeg(file = paste('qr/', mat_num[i], '.jpeg', sep = ''), width = 500, height = 500)
+  qrcode_gen(paste(mat_num[i]))
+  dev.off()
 }
 after <- Sys.time()
 
