97e1d8f79d6c02555c782e09a40e33b81b9c1950
Jonathan <Jonathan@berrisch.biz>
2019-09-24 09:28:14 +0200

    Update qr generator code

--- QR-Generator.R
+++ QR-Generator.R
@@ -20,18 +20,18 @@ students_moodle <- students_moodle %>% dplyr::select(Nutzer, Matrikelnummer, E.M
 
 students_accepted$Matrikelnummer <- as.numeric(students_accepted$Matrikelnummer)
 
-Mails_and_Numbers <- semi_join(students_moodle, students_accepted, by = "Matrikelnummer")
-Missings <- anti_join(students_accepted,students_moodle, by = "Matrikelnummer")
+Full <- right_join(students_moodle, students_accepted, by = "Matrikelnummer")
 
-Full <- full_join(Mails_and_Numbers, Missings)
+file_path <- rstudioapi::selectDirectory()
 
+Full$path <- paste(file_path, "/", Full$Matrikelnummer, ".jpeg", sep = "")
 
-before <- Sys.time()
-for (i in 1:length(mat_num)) {
-  jpeg(file = paste('qr/', mat_num[i], '.jpeg', sep = ''), width = 500, height = 500)
-  qrcode_gen(paste(mat_num[i]))
+dir.create(file_path, recursive = T, showWarnings = F)
+
+for (i in 1:nrow(Full)){
+  jpeg(file = Full$path[i], width = 500, height = 500)
+  qrcode_gen(paste(Full$Matrikelnummer[i]))
   dev.off()
 }
-after <- Sys.time()
 
-after - before
+write.csv2(file = paste(file_path, "/table.csv", sep = ""), Full, row.names = F)
